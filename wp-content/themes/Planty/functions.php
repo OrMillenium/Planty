1d6bcaf3395112c505bca170b22719f877678fcc
Aurore <aurore.esslinger@gmail.com>
2023-05-31 15:38:58 +0200

    hook

--- wp-content/themes/Planty/functions.php
+++ wp-content/themes/Planty/functions.php
@@ -2,14 +2,13 @@
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles()
 {
-    wp_enqueue_style('parent-style',get_template_directory_uri() . '/style.css');
+    
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/style.css', array(), filemtime(get_stylesheet_directory() . '/css/style.css'));
 }
-
-
 
 /*HOOK FILTERS...................*/
 
-add_filter( 'wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2 );
+add_filter( 'wp_nav_menu_items', 'add_extra_item_to_nav_menu',10,2);
 function add_extra_item_to_nav_menu( $items, $args ) {
     
     if (!is_user_logged_in() && $args->menu == 690) {
