e76be64b10e9f7ab69c3222227fb8d6057caef1b
Aurore <aurore.esslinger@gmail.com>
2023-06-02 12:49:08 +0200

    fonction

--- wp-content/themes/Planty/functions.php
+++ wp-content/themes/Planty/functions.php
@@ -2,9 +2,10 @@
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles()
 {
-    
-    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/style.css', array(), filemtime(get_stylesheet_directory() . '/css/style.css'));
+    wp_enqueue_style('parent-style',get_template_directory_uri() . '/style.css');
 }
+    
+    
 
 /*HOOK FILTERS...................*/
 
@@ -16,3 +17,4 @@ function add_extra_item_to_nav_menu( $items, $args ) {
     }
     return $items;
 }
+
