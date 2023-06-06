c0ba9b31921b2d264d6bafde07ea977d14994508
Aurore <aurore.esslinger@gmail.com>
2023-06-06 13:31:00 +0200

    hook

--- wp-content/themes/Planty/functions.php
+++ wp-content/themes/Planty/functions.php
@@ -9,12 +9,18 @@ function theme_enqueue_styles()
 
 /*HOOK FILTERS...................*/
 
-add_filter( 'wp_nav_menu_items', 'add_extra_item_to_nav_menu',10,2);
-function add_extra_item_to_nav_menu( $items, $args ) {
-    
-    if (!is_user_logged_in() && $args->menu == 690) {
-        $items .= '<li><a href="' . get_permalink( wc_get_page_id( 'myaccount' ) ) . '">Sign in  /  Register</a></li>';
+
+add_filter( 'wp_nav_menu_items', 'wpb_admin_menu', 10, 2 );
+function wpb_admin_menu( $items, $args ) {
+
+    // Vérifie si l'utilisateur est connecté, a la capacité de gérer les options et que le menu est situé dans l'emplacement "primary"
+    if ( is_user_logged_in() && current_user_can( 'manage_options' ) && $args->theme_location == 'primary' ) {
+
+        // Si toutes les conditions sont remplies, on ajoute un lien vers l'interface d'administration de WordPress à la fin de la liste de menu
+        $items .= '<li><a href="' . admin_url() . '">Admin</a></li>';
     }
+
+    // La fonction doit retourner le contenu des éléments du menu, donc on retourne la variable $items
     return $items;
 }
 
